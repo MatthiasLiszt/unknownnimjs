db93219d4f838ce7288b982a4e2d1f3d7b1bdbcf
Nadezxda <nadja@malinkakalinka>
2017-07-02 20:46:22 +0200

    AI? updated

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # unknownnimjs
+
 unknown nim game variation implemented in react.js ... tried to be as functional as I could 
+(this means I avoided successfully any kind of classical loop)
 
 ## installation
 
@@ -7,3 +9,11 @@ this uses webpacked react.js , so please ignorse bundle.js and go straight for t
 
 if everthing is setup correctly and the package.json is error-free you can run it with 'npm install' and then 'npm run'and 
 then you have to still load the page which is /src/client/index.html ... or just wait until the codepen version is ready
+
+## How To Play
+
+The object of the game is to make the last possible move. Every move you occupy fields.
+There are a total of 16 fields. You can select up to 3 fields in a row or column by 
+clicking on two different fields or you can select one by clicking on one field twice. 
+
+Have fun ! 
--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -6,8 +6,13 @@ const xpng="x.png";
 const opng="o.png";
 const errorI="error.png";
 
+// gameBoard and player Settings 
 let gameBoard=Array(16).fill('0');
+let playerMode='2';
+let playerColor='x';
 
+// ----------------------
+// class Field ; requires global variables playerColor ...
 class Field extends React.Component {
   constructor(props){
     super(props);
@@ -65,9 +70,13 @@ class Field extends React.Component {
   }
   switchA(){
    gameBoard=gameBoard.map(function(x){
-                             if(x=='a'){return 'x';}    
+                             if(x=='a'){return playerColor;}    
                              else {return x;}       
              });
+  }
+  switchColor(){
+   if(playerColor=='x'){playerColor='o';return true;}
+   if(playerColor=='o'){playerColor='x';return true;}
   }
   toomuch(){
     let i=0;
@@ -87,15 +96,25 @@ class Field extends React.Component {
     var iD=parseInt(this.props.id,10);
     console.log("id "+iD);     
     if(gameBoard[iD]=='a') 
-     {gameBoard[iD]='x';
-      this.setState(function(){ return {img: xpng}; });        
+     {gameBoard[iD]=playerColor;
+      if(playerColor=='x') 
+       {this.setState(function(){ return {img: xpng}; });
+        if(playerMode=='2'){this.switchColor();}
+        if(playerMode=='1'){makeMove();}
+       }
+      else  
+       {this.setState(function(){ return {img: opng}; });
+        if(playerMode=='2'){this.switchColor();}  
+        if(playerMode=='1'){makeMove();} 
+       }                
      } 
     if(gameBoard[iD]=='0') 
      {gameBoard[iD]='a';}
     if(this.checkLong())
      {console.log("checkLong passed");
       this.switchA();
-      reFresh();
+      if(playerMode=='2'){this.switchColor();reFresh();} 
+      if(playerMode=='1'){makeMove();}
      }      
     this.toomuch();
     console.log(JSON.stringify(gameBoard));
@@ -121,6 +140,7 @@ class Field extends React.Component {
    } 
  
 }
+//----------------
 
 
 class PlayField extends React.Component {
@@ -150,6 +170,27 @@ class PlayField extends React.Component {
 }
 
 
+class PlayerButton extends React.Component{
+  constructor(props){
+   super(props);
+   this.handleClick=this.handleClick.bind(this);
+  } 
+
+  handleClick(){
+    playerMode=this.props.mode;
+    console.log("player Mode = "+playerMode);
+    reFresh();
+  }
+ 
+  render(){
+   if(this.props.mode=='1')
+    {return <button onClick={this.handleClick}>One Player</button>;} 
+   if(this.props.mode=='2')
+    {return <button onClick={this.handleClick}>Two Players</button>;} 
+  }
+}
+
+
 class App extends React.Component {
   
   constructor(props){
@@ -160,11 +201,24 @@ class App extends React.Component {
   render () {
     const hl = <p className="hl"> Unknown Abandoned Nim Variation </p>;
     const playField=<PlayField />;  
+    
     return <div className="app">{hl}{playField}</div>;
+    
+    
   }
 }
 
-render(<App/>, document.getElementById('app'));
+class PlayerMode extends React.Component{
+  render()
+   {const hl = <p className="hl"> Unknown Abandoned Nim Variation </p>;
+    const pMode=<div className="bline"><PlayerButton mode="1"/><PlayerButton mode="2"/></div>;
+    
+    return <div>{hl}{pMode}</div>;
+   }
+}
+
+
+render(<PlayerMode/>, document.getElementById('app')); // Program Entry
 
 function reFresh(){
   console.log("refresh activated");
@@ -176,3 +230,94 @@ function reFresh(){
   render(<App/>, document.getElementById('app'));
 }
 
+//------------------------
+// this is supposed to be the great Game AI 
+function makeMove(){
+  let comColor,x;
+  let ef,cq;
+  
+  if(playerColor=='x'){comColor='o';}
+  if(playerColor=='o'){comColor='x';}
+
+  // returns -1 if there is none , otherwise the index of the field
+  // requires global variable gameBoard
+  function emptyField(){
+    let x;
+ 
+    function a(i){
+      if(gameBoard[i]=='0'){x=i;return i;}
+      if(i>gameBoard.length){x=-1;return -1;}
+      a(i+1);
+    }  
+    a(0);    
+    return x;
+  }
+
+  // requires global variable gameBoard
+  // returns false on error
+  function makeRandomMove(){
+    var x=Math.round(Math.random()*gameBoard.length); 
+    var e=emptyField();  
+    
+    if(gameBoard[x]=='0')
+      {gameBoard[x]=comColor;
+       return true;}
+    if(emptyField()<0){return false;}
+    gameBoard[e]=comColor;
+    //makeRandomMove(); 
+  }
+  
+  // requires global variable gameBoard
+  // returns number of empty fields 
+  function getEmptyFieldNumber(){
+    var x=gameBoard.reduce(function(t,f){
+                     if(f=='0'){return t+1;}
+                     else {return t;}
+                    },0);       
+    return x;       
+  }
+
+  x=emptyField();
+  console.log("emptyField  x returned "+x);
+  ef=getEmptyFieldNumber();
+  console.log("number of empty fields "+ef);
+  if(ef%2==0)
+   {console.log("computer might lose!");
+    cq=CounterStrike(comColor);//returns true when successfull
+    if(cq){console.log("counterstrike successful!");reFresh();}
+   }
+  if((x>=0)&&!cq){makeRandomMove();reFresh();}
+  if(x<0){console.log("Game Over !!!");}
+
+}
+
+
+function CounterStrike(x){
+   
+  console.log("counter strike activated"); 
+
+  if(gameBoard[0]=='0'&&gameBoard[1]=='0'){gameBoard[0]=x;gameBoard[1]=x;return true;}
+  if(gameBoard[0]=='0'&&gameBoard[4]=='0'){gameBoard[0]=x;gameBoard[4]=x;return true;}
+  
+  if(gameBoard[1]=='0'&&gameBoard[5]=='0'){gameBoard[1]=x;gameBoard[5]=x;return true;}
+  if(gameBoard[2]=='0'&&gameBoard[6]=='0'){gameBoard[2]=x;gameBoard[6]=x;return true;}
+  if(gameBoard[3]=='0'&&gameBoard[2]=='0'){gameBoard[3]=x;gameBoard[2]=x;return true;}
+  if(gameBoard[3]=='0'&&gameBoard[7]=='0'){gameBoard[3]=x;gameBoard[7]=x;return true;}
+ 
+  if(gameBoard[4]=='0'&&gameBoard[5]=='0'){gameBoard[4]=x;gameBoard[5]=x;return true;}
+  if(gameBoard[8]=='0'&&gameBoard[9]=='0'){gameBoard[8]=x;gameBoard[9]=x;return true;}
+  if(gameBoard[7]=='0'&&gameBoard[6]=='0'){gameBoard[7]=x;gameBoard[6]=x;return true;}
+  if(gameBoard[11]=='0'&&gameBoard[10]=='0'){gameBoard[11]=x;gameBoard[10]=x;return true;}
+ 
+  if(gameBoard[12]=='0'&&gameBoard[13]=='0'){gameBoard[12]=x;gameBoard[13]=x;return true;}
+  if(gameBoard[12]=='0'&&gameBoard[8]=='0'){gameBoard[12]=x;gameBoard[8]=x;return true;}
+  if(gameBoard[13]=='0'&&gameBoard[9]=='0'){gameBoard[13]=x;gameBoard[9]=x;return true;}
+    
+  if(gameBoard[14]=='0'&&gameBoard[10]=='0'){gameBoard[14]=x;gameBoard[10]=x;return true;}
+  if(gameBoard[15]=='0'&&gameBoard[14]=='0'){gameBoard[15]=x;gameBoard[14]=x;return true;}
+  if(gameBoard[15]=='0'&&gameBoard[11]=='0'){gameBoard[15]=x;gameBoard[11]=x;return true;}
+
+  return false;  
+}
+
+//-----------------------------
